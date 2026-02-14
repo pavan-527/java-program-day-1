905e7d4348a7cb362bb3d8004800cd5147da6647
pavan-527 <pk4620409@gmail.com>
2026-02-14 11:50:51 +0530

    Add files via upload

---
+++ Alpha.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Alpha {
+     public static void main(String[] var0) {
+      Scanner sc= new Scanner(System.in);
+      char alpha=sc.next().charAt(0);
+      if (alpha=='a'||alpha=='e'||alpha=='i'||alpha=='o'||alpha=='u')
+        System.out.println(alpha+"is a vowel");
+    else
+        System.out.println(alpha+"is a consonant"); 
+}
+}
