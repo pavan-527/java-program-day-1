1b7b5a35dd5fa9b9c443c989730ba6c57748b36c
pavan-527 <pk4620409@gmail.com>
2026-02-14 11:53:11 +0530

    Add files via upload

---
+++ Demo Word.java
@@ -0,0 +1,17 @@
+public class Demo Word {
+    public static void main(String[] args){
+    int a=24;
+    int b=20;
+    int c=35;
+    if(a>b){
+        System.out.println("The lagest number:"+a);
+    }
+    else if(b>c){
+        System.err.println("The largest number:"+b);
+    }
+    else{
+        System.err.println("The largest number:"+c);
+    }
+}
+
+}
