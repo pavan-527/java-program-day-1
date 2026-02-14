afa987f514c9d1e98b17780d6c224de8420a4a6d
pavan-527 <pk4620409@gmail.com>
2026-02-14 11:53:58 +0530

    Add files via upload

---
+++ Wild.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Wild {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%4==0)
+            System.out.println(a+"is a leap year");
+        else
+            System.out.println(a+"is not a leap year");
+    }
+}
