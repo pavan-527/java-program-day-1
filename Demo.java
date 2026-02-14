b963ea9ce060b4e54acfe46658a704e53ec246d6
pavan-527 <pk4620409@gmail.com>
2026-02-14 11:53:28 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo{
+    public static void main(String[] args){
+    int a=24;
+    int b=20;
+    int c=35;
+    if(a>b){
+        System.out.println("The lagest number:"+a);
+    }
+    else {
+        System.err.println("The largest number:"+b);
+    }
+}
+}
