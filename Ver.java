4f6d5c9466462a343a8aa16b24f97ac720271247
pavan-527 <pk4620409@gmail.com>
2026-02-28 13:40:38 +0530

    Add files via upload

---
+++ Ver.java
@@ -0,0 +1,12 @@
+public class Ver{
+    public static void main(String args[]){
+        int arr[]={22,33,44,55};
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+            System.out.println("arr:"+sum);
+        }
+
+    }
+}
